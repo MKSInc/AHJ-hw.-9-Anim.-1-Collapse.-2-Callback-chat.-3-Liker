2996632e6fda72e59a0ab4fac886e7e4a54dff7b
Maksim <pyshkov.maksim@gmail.com>
2021-07-29 12:33:27 +0300

    feat: task 1 completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-// eslint-disable-next-line no-console
-console.log('main.js run');
+import Collapse from '../components/Collapse/Collapse';
+
+// Если в качесте параметра в конструктор передать селектор или элемент,
+// то виджет по умолчанию будет добавлен в конец этого элемента через append.
+// Если виджет нужно добавить не в конец элемента, то можно это сделать вручную,
+// используя свойство виджета element.
+
+const contentEl = document.querySelector('.content');
+const widgetCollapse1 = new Collapse();
+contentEl.prepend(widgetCollapse1.element);
+
+const widgetCollapse2 = new Collapse({ parent: '.content' });
+widgetCollapse2.setText('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae inventore mollitia nesciunt non quos! Accusantium aperiam assumenda eaque impedit, in laborum libero nobis recusandae rem sunt totam, vel, vitae voluptate!');
